9f1839e6354e676b5d3d01c0a1a35352291478af
AsiA203 <aselia.azimkanova@gmail.com>
2024-11-06 19:23:07 -0600

    Created Folder for HW5 (Java class) and created Student.java class

--- src/main/java/Java/week_5/Student.java
+++ src/main/java/Java/week_5/Student.java
@@ -1,2 +1,30 @@
-package Java.week_5;public class Student {
+package Java.week_5;
+
+import javax.swing.*;
+
+public class Student {
+    private String name;
+    private int age;
+
+    public Student (String name, int age) throws Exception {
+        this.name = name;
+        this.age = age;
+
+        if (name == null || age < 18 || age > 60){
+            throw new Exception("Invalid Input");
+        }
+    }
+
+    //Custom exception
+    public void InvalidAgeException() throws Exception {
+        if (age < 18 || age > 60){
+            throw new Exception("Invalid Age Exception");
+        }
+    }
+
+    public void InvalidNameException() throws Exception{
+        if (name == null || name.equals("")){
+            throw new Exception("Invalid Name Exception");
+        }
+    }
 }
