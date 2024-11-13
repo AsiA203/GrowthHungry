11e78d9f6c3950e8a4c1128c04cc11a481b92f19
AsiA203 <aselia.azimkanova@gmail.com>
2024-11-12 22:04:15 -0600

    continuing week_6 hm

--- src/main/java/Java/week_6/GenericSorting.java
+++ src/main/java/Java/week_6/GenericSorting.java
@@ -1,7 +1,15 @@
 package Java.week_6;
 
+import static jdk.internal.org.jline.reader.impl.LineReaderImpl.CompletionType.List;
+
 public class GenericSorting {
-    public static <T> void sort(){
+    public static <T> void sort() {
         //TODO
+
+        private static <T > void swap (List < T > list,int i, int j){
+            T temp = list.get(i);
+            list.set(i, list.get(j));
+            list.set(j, temp);
+        }
     }
 }
