332909843c2dcf579a9fa08430b2a6e17b536dec
AsiA203 <aselia.azimkanova@gmail.com>
2024-12-18 22:56:36 -0600

    ToBinaryCode for Week8 of Math Course

--- src/main/java/DescreteMath/week_8/ExperimentToBinary.java
+++ src/main/java/DescreteMath/week_8/ExperimentToBinary.java
@@ -1,2 +1,48 @@
-package DescreteMath.week_8;public class ExperimentToBinary {
+package DescreteMath.week_8;
+
+import java.math.BigInteger;
+
+public class ExperimentToBinary {
+    public static void main(String[] args) {
+        BigInteger result = new BigInteger(String.valueOf(toBinary(284)));
+        System.out.println(result);
+    }
+
+    public static StringBuilder toBinary(long number) {
+        if (number == 0){
+            return new StringBuilder();
+        }
+
+        StringBuilder result = toBinary(number/2);
+        result.append(number % 2);
+
+        return result;
+    }
+
+    //    public static StringBuilder toBinary(long number) {
+//        StringBuilder result = new StringBuilder();
+//        if (number == 0){
+//            return result;
+//        }
+//
+//        result.append(number % 2);
+//
+//        StringBuilder recursiveResult = toBinary(number/2);
+//        recursiveResult.append(result);
+//
+//        return recursiveResult;
+//    }
+
+//    public static StringBuilder toBinary(long number){
+//        StringBuilder result = new StringBuilder();
+//        if (number == 0){
+//            return result;
+//        }
+//
+//        StringBuilder stepResult = new StringBuilder();
+//        stepResult.append(number % 2);
+//        result.append(stepResult);
+//
+//        return toBinary(number / 2);
+//    }
 }
